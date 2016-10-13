9dd7cc2d64b230a25c49ff953809dc5fdb7f3ad0
rRajivramachandran <rrajiv9802@gmail.com>
2016-10-13 20:35:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-
+import from stdio.h
 var app = express();
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {  
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/article-one',function(req,res){var nam=query("enter name");
+app.get('/article-one',function(req,res){var nam=prompt("enter name");
     
     
     
