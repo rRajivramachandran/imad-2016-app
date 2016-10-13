150bf3275d802b2cc3bf7121fe5e39aaf81fef3c
rRajivramachandran <rrajiv9802@gmail.com>
2016-10-13 20:18:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.get('/article-one',function(req,res){var nam=query("enter name");
     
     
     
-    res.sendFile(path.join(__dirname, 'ui','article-one.html'));
+    //res.sendFile(path.join(__dirname, 'ui','article-one.html'));
     res.send(nam);
     
     
