8a74f5c2517735c3f6e2bd258b951ad87ae9ba05
rRajivramachandran <rrajiv9802@gmail.com>
2016-10-13 22:55:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,6 @@
 console.log('Loaded!');
 var rr= document.getElementById("so");
 rr.innerHTML="kk";
-var mad= document.getElementById("madi");
-mad.onClick= function() {
+var mad = document.getElementById("madi");
+mad.onclick= function() {
 mad.style.marginLeft= "100px";};
