a2376babfefe6ed713a2f3401f4ee523bf3748eb
rRajivramachandran <rrajiv9802@gmail.com>
2016-10-20 22:14:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,7 @@ var mad = document.getElementById("madi");
 
 function moveright(it) {
     var margin = 0;
-    if(margin<300)
+    if(margin<400)
    {margin =margin+5;
     it.style.marginLeft=margin+"px";}
     
