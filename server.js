11a72fb625b4f3a77e5f93136c6283cfda77a3ca
rRajivramachandran <rrajiv9802@gmail.com>
2016-10-22 00:45:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.get('/article-one',function(req,res){
     
     
 });
-var count=0;
+
 app.get('/article-two',function(req,res){
    res.sendFile(path.join(__dirname, 'ui','article-two.html'));
     
