d24d6679df39acd6ad77faa91396ab48cb81d78a
rRajivramachandran <rrajiv9802@gmail.com>
2016-10-22 14:17:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -21,8 +21,8 @@ var count=0;
  qw.onclick=function(){ count=count+1;
 var hw=document.getElementById("h");
  hw.innerHTML=count.toString();};
- var j=document.getElementById("name");
- var k=j.value;
+ var p=document.getElementById("name");
+ var k=p.value;
  var d= k;
  var t= doument.getElementById("tt");
  t.onclick= function(){
